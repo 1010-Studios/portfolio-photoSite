cf8705c8089837b8af74f09fed0e4dd1a764b2bf
Tyler Lowes <Tyler.Lowes@gmail.com>
2021-02-24 11:23:34 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// window.addEventListener('load', () => {
+//Navbar Handling
+const _nav = document.querySelector('.nav');
+const _toggleCollapse = document.querySelector('.toggle-collapse');
+
+_toggleCollapse.addEventListener('click', () => {
+	_nav.classList.toggle('collapse');
+});
+
+//photo Carousel
+let slideIndex = 1;
+const slides = document.querySelectorAll('.photo-content');
+
+moveSlide = (e) => {
+	e.preventDefault();
+	const n = parseInt(e.target.value);
+	console.log(e.target.value);
+	slideIndex += n;
+	showSlide(slideIndex);
+};
+
+showSlide = (n) => {
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].classList.remove('photo-focus');
+	}
+
+	slides[slideIndex - 1].classList.add('photo-focus');
+	console.log(slides);
+};
+
+slideShow = () => {
+	slideIndex++;
+	showSlide(slideIndex);
+};
+
+setInterval(slideShow, 4000);
+
+const _carouselBtns = document.querySelectorAll('.btn-carousel');
+_carouselBtns.forEach((item) => {
+	item.addEventListener('click', moveSlide);
+});
+// });
